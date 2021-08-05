7a04fb053e18794d04059b1018d9a21fe1821c69
Niels Korschinsky <niels.korschinsky@mni.thm.de>
2021-08-05 20:43:40 +0200

    add user DTO, get some generator feeders

--- src/test/scala/RunScenarios.scala
+++ src/test/scala/RunScenarios.scala
@@ -1,49 +1,34 @@
 package de.thm.mni.microservices.gruppe6
 
-import scala.concurrent.duration._
 import scala.util.Random
-
 import io.gatling.core.Predef._
+import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
 import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
 import io.gatling.jdbc.Predef._
+
+import java.time.LocalDate
+import java.time.format.{DateTimeFormatter, DateTimeFormatterBuilder}
 
 class RunScenarios extends Simulation {
 
-  val httpProtocol = http
+  val httpProtocol: HttpProtocolBuilder = http
     .baseUrl("http://localhost:8069")
-    .inferHtmlResources(
-      BlackList(
-        """.*\.js""",
-        """.*\.css""",
-        """.*\.gif""",
-        """.*\.jpeg""",
-        """.*\.jpg""",
-        """.*\.ico""",
-        """.*\.woff""",
-        """.*\.woff2""",
-        """.*\.(t|o)tf""",
-        """.*\.png""",
-        """.*detectportal\.firefox\.com.*"""
-      ),
-      WhiteList()
-    )
+    .acceptHeader(Map(
+      "Content-Type" -> "application/json"
+    ))
     .userAgentHeader(
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
     )
 
-  val randomQuestionBody =
-    Iterator.continually(
-      Map("randomRoomName" -> (Random.alphanumeric.take(200).mkString))
-    )
-
   val sessionHeaders = Map(
-    "Authorization" -> "${authToken}",
     "Content-Type" -> "application/json"
   )
 
-  val headers_0 = Map(
-    "Content-Type" -> "application/json"
+  val jwtHeader = Map(
+    "Authorization" -> "${authToken}"
   )
+
 
   /*
   Scenario base:
@@ -69,9 +54,88 @@ class RunScenarios extends Simulation {
   mach neues Issue
    */
 
-  var randomProjectNameFeeder = Iterator.continually(
-    Map("randomProjectName" -> (Random.alphanumeric.take(10).mkString))
-  )
+  var randomString: Iterator[String] = Iterator.continually(Random.alphanumeric.take(Random.between(5,20)).mkString)
+
+  var randomFutureDate: Iterator[String] = Iterator.continually(LocalDate.now()
+    .plusDays(Random.nextInt(600))
+    .format(DateTimeFormatter.ofPattern("dd.MM.yyyy")))
+
+  var randomPastDate: Iterator[String] = Iterator.continually(LocalDate.now()
+    .minusDays(Random.nextInt(100*365))
+    .format(DateTimeFormatter.ofPattern("dd.MM.yyyy")))
+
+  var randomStringFeeder: Iterator[Map[String, String]] = Iterator.continually(
+    Map("randomString" -> randomString.next()))
+
+  var randomFutureDateFeeder: Iterator[Map[String, String]] = Iterator.continually(
+    Map("randomFutureDate" -> randomFutureDate.next()))
+
+  var randomPastDateFeeder: Iterator[Map[String, String]] = Iterator.continually(
+    Map("randomPastDate" -> randomPastDate.next()))
+
+  def newUserFeeder(role:String): Iterator[Map[String, String]] = {
+    Iterator.continually(Map(
+      "username" -> randomString.next(),
+      "password" -> randomString.next(),
+      "name" -> randomString.next(),
+      "lastName" -> randomString.next(),
+      "email" -> (randomString.next() + "@mni.thm.de"),
+      "dateOfBirth" -> randomPastDate.next(),
+      "globalRole" -> role
+    ))
+  }
+
+
+  object User {
+    def login(username: String, password: String): ChainBuilder = {
+      exec(
+        http("loginUser")
+          .get("/login")
+          .basicAuth(username,password)
+          .check(status.is(200))
+          .check(header("authorization").exists)
+          .check(
+            header("authorization").saveAs("authToken")
+          )
+      )
+    }
+
+    def create(role: String): ChainBuilder = {
+      feed(newUserFeeder(role))
+      .exec(
+        http("createProjectHTTP")
+          .post(s"/api/users/")
+          .headers(jwtHeader)
+          .body(ElFileBody("bodies/userDTO.json")).asJson
+          .check(status.is(201))
+          .check(bodyString.saveAs("BODY"))
+      )
+    }
+  }
+  object Project {
+    def create(projectName: String): ChainBuilder = {
+      exec(
+        http("createProjectHTTP")
+          .post(s"/api/projects/${projectName}")
+          .headers(jwtHeader)
+          .headers(sessionHeaders)
+          .check(status.is(201))
+      )
+    }
+
+  }
+
+  val scenarioLoginPeter: ScenarioBuilder =
+    scenario("LoginAsPeter")
+    .exec(User.login("Peter","password"))
+
+  val scenarioPeterCreateProject: ScenarioBuilder =
+    scenario("CreateProjectAsPeter")
+    .exec(User.login("Peter","password"))
+    .exec(scenarioLoginPeter, Project.create("Test"))
+
+
+  /*
   val scn1 = scenario("LoginAsPeter").exec(
     feed(randomProjectNameFeeder)
       .exec(
@@ -92,6 +156,8 @@ class RunScenarios extends Simulation {
           .check(status.is(201))
       )
   )
+
+   */
 
   /*val scn1 = scenario("LoginAsGuestAndCreateRoom").exec(
     feed(randomRoomNameFeeder)
@@ -159,7 +225,7 @@ class RunScenarios extends Simulation {
   //inject(atOnceUsers(20))
   //      .andThen(scn2.inject(constantUsersPerSec(5).during(1.minute).randomized))
   setUp(
-    scn1
+    scenarioLoginPeter
       .inject(atOnceUsers(5))
   )
     .protocols(httpProtocol)
